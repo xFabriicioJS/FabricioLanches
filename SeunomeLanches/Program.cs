2f39968ddbbd12899efb25f4d30f3bd14c06cd75
FABRICIO COSTA MONTEIRO <fabriciocostamonteiro@hotmail.com>
2022-04-05 11:50:27 -0300

    Adicionar arquivos de projeto.

---
+++ SeunomeLanches/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeunomeLanches
+{
+    internal class Program
+    {
+        struct Pedido
+        {
+            public string Id;
+            public string Value;
+            public string Name;
+
+
+            public Pedido(string id, string value, string name)  //Construtor da Struct
+            {
+                Id = id;
+                Value = value;
+                Name = name;
+
+            }
+
+           
+
+        }
+
+
+            static void Main(string[] args)
+        {
+          Pedido lanche1 = new Pedido("01", "5", "X-Salada");
+            Pedido lanche2 = new Pedido("02", "8", "X-Burger");
+            Pedido lanche3 = new Pedido("03", "3", "HotDog");
+
+            //Criando fila com a struct
+
+            Queue<Pedido> fila = new Queue<Pedido>();
+            fila.Enqueue(lanche1);
+            fila.Enqueue(lanche2);
+            fila.Enqueue(lanche3);
+
+            foreach (object pedido in fila)
+                Console.WriteLine($"Seu pedido é :"); 
+            Console.ReadKey();
+            
+        }
+
+        
+    }
+}
